4aff5986b35cf4e1ecf342f6691decb8f8bbe694
Ignacio-Molina-0804 <molinagranda@gmail.com>
2024-08-18 16:57:23 -0300

    JavaScriptPruebas

---
+++ JavaScriptPruebas/variables.js
@@ -0,0 +1,20 @@
+//String
+
+var bootcamp = "Henry";
+
+//Numeros enteros, decimales y negativos
+
+var numeroEntero = 17;
+
+// booleanos true o false
+
+var boolean = true;
+
+// Undefined
+
+var indefinido;
+console.log(indefinido)
+
+// Null
+
+var nulo = null;
